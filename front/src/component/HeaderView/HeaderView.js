01b200a4b20107bd637069fbb2c614b73ac5ba84
SWTangKai <tangkailh@outlook.com>
2018-05-13 22:29:19 +0800

    a litter fix

--- front/src/component/HeaderView/HeaderView.js
+++ front/src/component/HeaderView/HeaderView.js
@@ -1,25 +1,28 @@
-import { Loader, log } from "../../utils/utils";
+import {
+    Loader,
+    log
+} from "../../utils/utils";
 
 import "./header.css";
 
-export default function(headerDom, changeCallBack) {
+export default function (headerDom, changeCallBack) {
 
     let dataset_dropdown_menu = $("#dataset-dropdown-menu");
     Loader.json("graph/").then(graphNameList => {
         graphNameList.forEach(e => {
-            dataset_dropdown_menu.append("<li><a href='#' id='" + e +"' value = '" + e + "'>" + e + "</a><li>");
-            $("#" + e).click(function(){
+            dataset_dropdown_menu.append("<li><a class='dropdown open' href='#' id='" + e + "' value = '" + e + "'>" + e + "</a><li>");
+            $("#" + e).click(function () {
                 changeCallBack($(this).attr("value"))
             })
         });
     })
 
     let select_dropdown_menu = $("#select-dropdown-menu"),
-    select_group = ['s1','s2','s3','s4'];
+        select_group = ['s1', 's2', 's3', 's4'];
 
     select_group.forEach(e => {
-        select_dropdown_menu.append("<li><a href='#' id='" + e +"' value = '" + e + "'>" + e + "</a><li>");
-        $("#" + e).click(function(){
+        select_dropdown_menu.append("<li><a href='#' id='" + e + "' value = '" + e + "'>" + e + "</a><li>");
+        $("#" + e).click(function () {
             changeCallBack($(this).attr("value"))
         })
     })
